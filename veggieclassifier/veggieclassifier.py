d9d998d8663827bd981226ec4505b2b5b4f44b5a
codyowl <codyowl@gmail.com>
2017-05-13 17:37:18 +0530

    Veggieclassifier added :rocket:

---
+++ veggieclassifier/veggieclassifier.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as img
+import numpy as np
+from scipy.misc import imresize
+
+%matplotlib inline
+
+import os
+from os import listdir
+from os.path import isfile, join
+import shutil
+import stat
+import collections
+from collections import defaultdict
+
+from ipywidgets import interact, interactive, fixed
+import ipywidgets as widgets
+
+import h5py
+from sklearn.model_selection import train_test_split
+from keras.utils.np_utils import to_categorical
+from keras.applications.inception_v3 import preprocess_input
+from keras.models import load_model
