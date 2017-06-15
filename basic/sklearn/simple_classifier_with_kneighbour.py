a17dfb1ab7611ffc0823f26cca4aa06e0bf03105
codyowl <codyowl@gmail.com>
2017-06-15 22:56:14 +0530

    Kickstared a simple classifier with kneighbourclassifier :rocket:

---
+++ basic/sklearn/simple_classifier_with_kneighbour.py
@@ -0,0 +1,18 @@
+from sklearn import datasets
+iris = datasets.load_iris()
+
+x = iris.data
+y = iris.target
+
+from sklearn.cross_validation import train_test_split
+x_train, x_test, y_train, y_train = train_test_split(x, y, test_size=.5)
+
+from sklearn.neighbors import KNeighborsClassifier
+my_classifier = KNeighborsClassifier()
+
+my_classifier.fit(x_train, y_train)
+
+predictions = my_classifier.predict(x_test)
+
+from sklearn.metrics import accuracy_score
+print accuracy_score(y_test, predictions)
